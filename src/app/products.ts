8d618903b6a299790cc3a595e65af42fa5de6d98
sandaruchamika <chamikasandaru01@gmail.com>
2021-04-21 09:15:01 +0530

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Phone XL',
+        price: 799,
+        description: 'Nice phone'
+    },
+    {
+        id: 2,
+        name: 'Phone XL',
+        price: 799,
+        description: 'Nice phone'
+    },
+    {
+        id: 3,
+        name: 'Phone XL',
+        price: 500,
+        description: 'Nice phone'
+    }
+];
